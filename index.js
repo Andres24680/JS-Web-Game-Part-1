450aae4f14d215b5ec94c507371386622d037ad1
Andres24680 <tasboyandresrestrepo@aol.com>
2022-10-22 16:20:16 -0400

    event listner

--- index.js
+++ index.js
@@ -11,6 +11,8 @@
 //pineTree.stytle.left= '450px'
 //pineTree.style.bottom= '200px'
 //document.body.append(pineTree)
+
+
 function newImage(url, left, bottom){
     let object = document.createElement('img')
     object.src = url
@@ -20,11 +22,43 @@ function newImage(url, left, bottom){
     document.body.append(object)
     return object
 }
+
+
+
+
+newImage('assets/green-character.gif', 100, 100)
+newImage('assets/tree.png', 200, 300)
+newImage('assets/pillar.png', 350, 100)
+newImage('assets/pine-tree.png', 450, 200)
+newImage('assets/crate.png', 150, 200)
+newImage('assets/well.png', 500, 425)
+
+
+
+
+newItem('assets/sword.png', 500, 405)
+newItem('assets/sheild.png', 165, 185)
+newItem('assets/staff.png', 600, 100)
+
+
+function newItem(url, left, bottom){
+    let object = newImage(url, left, bottom)
+
+    object.addEventListener('dblclick', () => {
+        object.remove()
+    })
 }
 
-newImage('assets/green-character.gif', '100px', '100px')
-newImage('assets/pine-tree.png', '450px', '200px')
-newImage('assets/tree.png', '200px', '300px')
-newImage('assets/pillar.png', '350px', '100px')
-newImage('assets/crate.png', '150px', '200px')
-newImage('assets/well.png', '500px', '425px')
+//('assets/grass.png', 0, horizon, window.innerWidth/100, heightOfGrass/100)
+
+//let sword = document.createElement('img')
+//sword.src = 'assets/sword.png'
+//sword.position = 'fixed'
+//sword.left = '500px'
+//sword.bottom = '405px'
+//document.body.append(sword)
+
+
+
+
+       
